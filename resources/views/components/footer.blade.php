22637a98dc5299ca00e0510fe3a66137ebdd1d4d
Ninh2577 <hoangninh2577@gmail.com>
2026-06-23 16:49:41 +0700

    Bỏ đi mục Email có trong Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -88,14 +88,7 @@
                                 </span>
                                 <a href="{{ $phoneHref }}" class="transition hover:text-[#29b8ff]">{{ $phoneDisplay }}</a>
                             </li>
-                            <li class="flex items-center gap-3">
-                                <span class="text-[#29b8ff]">
-                                    <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                                    </svg>
-                                </span>
-                                <a href="{{ $emailHref }}" class="break-all transition hover:text-[#29b8ff]">{{ $email }}</a>
-                            </li>
+
                         </ul>
                     </section>
 
