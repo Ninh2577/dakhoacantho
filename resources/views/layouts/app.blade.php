66bbbaad2e19a9f5d7025886f7bc3c6411b75ab0
Ninh2577 <hoangninh2577@gmail.com>
2026-06-25 15:18:46 +0700

    Thêm icon và ảnh đại diện bỏ mục Medical clinic

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('meta')
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
